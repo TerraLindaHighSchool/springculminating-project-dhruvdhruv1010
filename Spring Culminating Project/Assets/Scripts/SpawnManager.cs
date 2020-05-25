802b1277351a3f7c4d92dd25485206bf75da714a
dhruvdhruv1010 <59099019+dhruvdhruv1010@users.noreply.github.com>
2020-05-24 22:54:29 -0700

    Car Spawner almost done

---
+++ Spring Culminating Project/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] carPrefabs;
+    private float spawnRangeX = 21;
+    private float spawnPosZ = 59;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomCar", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnRandomCar()
+    {
+        int carIndex = Random.Range(0, carPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(carPrefabs[carIndex], spawnPos, carPrefabs[carIndex].transform.rotation);
+
+    }
+}
