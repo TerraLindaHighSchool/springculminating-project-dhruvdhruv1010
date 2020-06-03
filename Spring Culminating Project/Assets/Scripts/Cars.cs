b9ec9799d8f00c7886729a9ebd65766269dbcfa0
dhruvdhruv1010 <59099019+dhruvdhruv1010@users.noreply.github.com>
2020-06-02 23:57:03 -0700

    Restructuring Scripts

---
+++ Spring Culminating Project/Assets/Scripts/Cars.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class Cars : MonoBehaviour
+{
+    public bool isGameActive;
+    public GameObject[] carPrefabs;
+    private float spawnRangeX = 21;
+    private float spawnPosZ = 55;
+    private float startDelay = 2;
+    private float spawnInterval = 0.7f;
+    private float lowerBound = -15;
+    public float speed = 40.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomCarLeftSide", startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomCarRightSide", startDelay, spawnInterval);
+    }
+
+    void StartGame()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        DestroyOutOfBounds();
+        MoveForward();
+    }
+    void DestroyOutOfBounds()
+    {
+        if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void MoveForward()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+    }
+
+    void SpawnRandomCarLeftSide()
+    {
+        if (isGameActive)
+        {
+            int carIndex = Random.Range(0, carPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, -2), 2, spawnPosZ);
+            Instantiate(carPrefabs[carIndex], spawnPos, carPrefabs[carIndex].transform.rotation);
+        }
+    }
+    void SpawnRandomCarRightSide()
+    {
+        if (isGameActive)
+        {
+            int carIndex = Random.Range(0, carPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(2, spawnRangeX), 2, spawnPosZ);
+            Instantiate(carPrefabs[carIndex], spawnPos, carPrefabs[carIndex].transform.rotation);
+        }
+    }
+}
--- Spring Culminating Project/Assets/Scripts/GameManager.cs
+++ Spring Culminating Project/Assets/Scripts/GameManager.cs
@@ -2,63 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
-    public GameObject[] carPrefabs;
-    private float spawnRangeX = 21;
-    private float spawnPosZ = 55;
-    private float startDelay = 2;
-    private float spawnInterval = 0.7f;
-    private float lowerBound = -15;
-    public float speed = 40.0f;
-    public bool isGameActive;
     public TextMeshProUGUI gameOverText;
+    private Button button;
+    public GameObject titleScreen;
 
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-        InvokeRepeating("spawnRandomCarLeftSide", startDelay, spawnInterval);
-        InvokeRepeating("spawnRandomCarRightSide", startDelay, spawnInterval);
+        button = GetComponent<Button>();
+        button.onClick.AddListener(StartGame);
+        StartGame();
     }
 
+    void StartGame()
+    {
+        isGameActive = true;
+        
+        titleScreen.gameObject.SetActive(false);
+    }
     // Update is called once per frame
     void Update()
     {
-        destroyOutOfBounds();
-        moveForward();
+        
     }
-    void destroyOutOfBounds()
-    {
-        if (transform.position.z < lowerBound)
-        {
-            Destroy(gameObject);
-        }
-    }
-    void moveForward()
-    {
-        transform.Translate(Vector3.right * Time.deltaTime * speed);
-    }
+    
     private void OnCollisionEnter(Collision collision)
     {
         isGameActive = false;
     }
-    void spawnRandomCarLeftSide()
-    {
-        if (isGameActive)
-        {
-            int carIndex = Random.Range(0, carPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, -2), 2, spawnPosZ);
-            Instantiate(carPrefabs[carIndex], spawnPos, carPrefabs[carIndex].transform.rotation);
-        }
-    }
-    void spawnRandomCarRightSide()
-    {
-        if (isGameActive)
-        {
-            int carIndex = Random.Range(0, carPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(2, spawnRangeX), 2, spawnPosZ);
-            Instantiate(carPrefabs[carIndex], spawnPos, carPrefabs[carIndex].transform.rotation);
-        }
-    }
+    
 }
